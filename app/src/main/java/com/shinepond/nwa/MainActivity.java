1b7c69ea420cff9d9b7ba58cae02737fa42da115
shinepond <shinepond@logisall.com>
2022-12-19 18:01:49 +0900

    add. 넥사크로 activity 플러그인 연결 추가

--- app/src/main/java/com/shinepond/nwa/MainActivity.java
+++ app/src/main/java/com/shinepond/nwa/MainActivity.java
@@ -1,19 +1,57 @@
 package com.shinepond.nwa;
 
-import com.nexacro.NexacroUpdatorActivity;
+import android.annotation.SuppressLint;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends NexacroUpdatorActivity {
+import androidx.annotation.NonNull;
 
-    public MainActivity() {
-        super();
-        setBootstrapURL( BuildConfig.HOST + "/_android_/start_android.json");
-        setProjectURL( BuildConfig.HOST + "/_android_/");
-    }
+import com.nexacro.NexacroActivity;
+
+
+public class MainActivity extends NexacroActivity {
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    StandardObject standardObj = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        Log.i(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    public void setPlugin(StandardObject obj) {
+        standardObj = obj;
+    }
+
+    @Override
+    @SuppressLint("NewApi")
+    protected void onResume()
+    {
+        super.onResume();
+        if(standardObj != null){
+            //화면의 on_resume 이벤트 함수호출
+            standardObj.send(
+                    StandardObject.CODE_SUCCESS,
+                    "화면 재활성화",
+                    standardObj.getActionString("_onresume")
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                /* TODO 권한 허용 시 */
+            } else {
+                /* 권한 거부 */
+                Toast.makeText(this, "권한요청을 거부했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
